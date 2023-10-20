893e3bfd45e1668788e2050bad4659e1ed0263a5
raneemhamarneh <raneem99hamarneh@gmail.com>
2023-10-20 09:38:36 +0300

    complete assignment

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Write your solution in this file!
+var customerName = 'bob';
+
+function getCustomerName() {
+    return customerName;
+}
+
+function upperCaseCustomerName() {
+    customerName = customerName.toUpperCase();
+}
+var bestCustomer;
+
+function setBestCustomer(){ 
+     bestCustomer = 'not bob';
+}
+
+function overwriteBestCustomer(){
+    bestCustomer = "maybe bob";
+}
+const leastFavoriteCustomer ='Assignment to constant variable.';
+
+function changeLeastFavoriteCustomer(){
+    leastFavoriteCustomer =" unsuccessfully tries to reassign the least favorite customer";
+    return leastFavoriteCustomer;
+}
